5fbdb7f1d431077eda9ae4a439327543e1e64a85
yashithaibmskillsbuild-netizen <yashithaibmskillsbuild@gmail.com>
2026-04-26 15:09:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+model = tf.keras.models.load_model("model.h5")
+
+class_names = ["Early Blight", "Late Blight", "Healthy"]
+
+st.title("Potato Leaf Disease Detection")
+
+uploaded_file = st.file_uploader("Upload a leaf image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).resize((224, 224))
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img_array = np.array(image) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    result = class_names[np.argmax(prediction)]
+
+    st.write("Prediction:", result)
