2f6fb747b5ec813dbd263568845529696bfb3caf
yashithaibmskillsbuild-netizen <yashithaibmskillsbuild@gmail.com>
2026-04-26 15:22:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("model.keras")
 
 class_names = ["Early Blight", "Late Blight", "Healthy"]
 
@@ -13,7 +13,7 @@ uploaded_file = st.file_uploader("Upload a leaf image", type=["jpg", "png", "jpe
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).resize((224, 224))
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image)
 
     img_array = np.array(image) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
@@ -21,4 +21,4 @@ if uploaded_file is not None:
     prediction = model.predict(img_array)
     result = class_names[np.argmax(prediction)]
 
-    st.write("Prediction:", result)
+    st.write(result)
