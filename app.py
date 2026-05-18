81ed1b2b6499f55ee294ff055ebee01812c5f250
yashithaibmskillsbuild-netizen <yashithaibmskillsbuild@gmail.com>
2026-05-18 18:31:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,12 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import gdown
-import os
 
-url = "https://drive.google.com/uc?id=1qU7yjbftkmyh6gvolJ20UTXyfJTqYAiY"
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
 
-model_path = "model.keras"
-
-if not os.path.exists(model_path):
-    gdown.download(url, model_path, quiet=False)
-
-model = tf.keras.models.load_model(model_path)
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 class_names = ["Early Blight", "Late Blight", "Healthy"]
 
@@ -24,15 +19,20 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file is not None:
+
     image = Image.open(uploaded_file).convert("RGB")
     image = image.resize((224, 224))
 
     st.image(image, caption="Uploaded Image")
 
-    img_array = np.array(image) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    img = np.array(image, dtype=np.float32)
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
 
-    prediction = model.predict(img_array)
+    interpreter.set_tensor(input_details[0]['index'], img)
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(output_details[0]['index'])
 
     predicted_class = class_names[np.argmax(prediction)]
     confidence = np.max(prediction) * 100
